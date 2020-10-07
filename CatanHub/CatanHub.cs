d3bdb25698b89a539d24cf938e7230dcf34e08a1
Joe Long <joelong@outlook.com>
2020-10-07 14:25:24 -0700

    Code Maid updates

--- CatanHub/CatanHub.cs
+++ CatanHub/CatanHub.cs
@@ -18,51 +18,110 @@ namespace CatanHub
     public interface ICatanClient
     {
         #region Methods
-        Task OnAck (CatanMessage message);
-        Task AllGames (List<GameInfo> games);
-        Task AllPlayers (ICollection<string> players);
-        Task AllMessages (List<CatanMessage> messages);
 
-        Task CreateGame (CatanMessage message);
+        Task AllGames(List<GameInfo> games);
 
-        Task DeleteGame (CatanMessage message);
+        Task AllMessages(List<CatanMessage> messages);
 
-        Task JoinGame (CatanMessage message);
-        Task RejoinGame (CatanMessage message);
+        Task AllPlayers(ICollection<string> players);
 
-        Task LeaveGame (CatanMessage message);
+        Task CreateGame(CatanMessage message);
 
-        Task ToAllClients (CatanMessage message);
+        Task DeleteGame(CatanMessage message);
 
-        Task ToOneClient (CatanMessage message);
-        Task ServiceError (CatanMessage message, string error);
+        Task JoinGame(CatanMessage message);
 
-        Task Pong ();
+        Task LeaveGame(CatanMessage message);
+
+        Task OnAck(CatanMessage message);
+
+        Task Pong();
+
+        Task RejoinGame(CatanMessage message);
+
+        Task ServiceError(CatanMessage message, string error);
+
+        Task ToAllClients(CatanMessage message);
+
+        Task ToOneClient(CatanMessage message);
 
         #endregion Methods
     }
 
     public class CatanHub : Hub<ICatanClient>
     {
+        #region Properties
+
         public static Games Games { get; set; } = new Games();
+
+        #endregion Properties
+
+
 
         #region Properties
 
-        private static ConcurrentDictionary<string, string> PlayerToConnectionDictionary = new ConcurrentDictionary<string, string>();
         private static ConcurrentDictionary<string, string> ConnectionToPlayerDictionary = new ConcurrentDictionary<string, string>();
-
+        private static ConcurrentDictionary<string, string> PlayerToConnectionDictionary = new ConcurrentDictionary<string, string>();
         private static string AllUsers { get; } = "{158B5187-959E-4A81-A8F9-CD9BE0D30300}";
 
         #endregion Properties
 
         #region Methods
 
+        public async Task GetAllGames()
+        {
+            var games = Games.GetGames();
+            await Clients.Caller.AllGames(games);
+        }
+
+        public async Task GetAllMessage(GameInfo gameInfo)
+        {
+            if (gameInfo == null) return;
+            Game game = Games.GetGame(gameInfo.Id);
+            if (game != null)
+            {
+                List<CatanMessage> messages = new List<CatanMessage>();
+                messages.AddRange(game.GameLog.ToArray());
+                await Clients.Caller.AllMessages(messages);
+            }
+        }
+
+        public async Task GetPlayersInGame(Guid gameId)
+        {
+            Game game = Games.GetGame(gameId);
+            if (game != null)
+            {
+                await Clients.Caller.AllPlayers(game.NameToPlayerDictionary.Keys);
+            }
+            else
+            {
+                await Clients.Caller.AllPlayers(new List<string>());
+            }
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, AllUsers);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, AllUsers);
+            if (ConnectionToPlayerDictionary.TryGetValue(Context.ConnectionId, out string playerName))
+            {
+                PlayerToConnectionDictionary.TryRemove(playerName, out string _);
+                ConnectionToPlayerDictionary.TryRemove(Context.ConnectionId, out string _);
+            }
+            await base.OnDisconnectedAsync(exception);
+        }
+
         public async Task Ping()
         {
             await Clients.Caller.Pong();
         }
 
-        public async Task PostMessage (CatanMessage message)
+        public async Task PostMessage(CatanMessage message)
         {
             if (message == null) return;
             if (message.GameInfo == null) return;
@@ -83,9 +142,11 @@ namespace CatanHub
                     message.MessageType = MessageType.BroadcastMessage;
                     await Clients.Group(gameId).ToAllClients(message);
                     break;
+
                 case MessageType.PrivateMessage:
                     await Clients.Group(gameId).ToAllClients(message);  // no private message for now
                     break;
+
                 case MessageType.CreateGame:
 
                     //
@@ -109,25 +170,28 @@ namespace CatanHub
                     await Clients.All.CreateGame(message);
 
                     break;
+
                 case MessageType.DeleteGame:
                     Games.DeleteGame(message.GameInfo.Id, out Game _);
                     //
                     //  tell *all* the clients that a game was deleted
                     await Clients.All.DeleteGame(message);
                     break;
+
                 case MessageType.RejoinGame:
                     //
                     //  this will only fail if the player is already there, but we don't care about that
                     game.NameToPlayerDictionary.TryAdd(message.From, new Player(game.GameLog));
 
                     await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
-                    
+
                     //
                     //  the difference between Join and Rejoin is that on Rejoin only the caller is told
                     //  the client will (presumably) make the rejoin seamless to the user
                     await Clients.Caller.RejoinGame(message);
 
                     break;
+
                 case MessageType.JoinGame:
 
                     //
@@ -139,21 +203,36 @@ namespace CatanHub
                     //  notify everybody else in the group that somebody has joined the game
                     await Clients.Group(gameId).JoinGame(message);
                     break;
+
                 case MessageType.LeaveGame:
                     game.NameToPlayerDictionary.TryRemove(message.From, out Player _);
                     _ = Groups.RemoveFromGroupAsync(Context.ConnectionId, gameId);
                     await Clients.Group(gameId).LeaveGame(message);
                     break;
+
                 case MessageType.Ack:
                     await Clients.Group(gameId).OnAck(message);
                     break;
+
                 default:
                     break;
             }
-
         }
 
-        public async Task SendError (CatanMessage message, string error)
+        public Task Register(string playerName)
+        {
+            PlayerToConnectionDictionary.AddOrUpdate(playerName, Context.ConnectionId, (key, oldValue) => Context.ConnectionId);
+            ConnectionToPlayerDictionary.AddOrUpdate(Context.ConnectionId, playerName, (key, oldValue) => playerName);
+            return Task.CompletedTask;
+        }
+
+        public Task Reset()
+        {
+            Games = new Games();
+            return Task.CompletedTask;
+        }
+
+        public async Task SendError(CatanMessage message, string error)
         {
             var errorMessage = new CatanMessage()
             {
@@ -163,85 +242,17 @@ namespace CatanHub
                 GameInfo = message.GameInfo,
                 ActionType = ActionType.Normal,
                 MessageId = message.MessageId
-
-
             };
             await Clients.Group(message.GameInfo.Id.ToString()).ToAllClients(errorMessage);
         }
 
-
-
-
-        public Task Reset ()
-        {
-            Games = new Games();
-            return Task.CompletedTask;
-        }
-
-        public Task Register (string playerName)
-        {
-            PlayerToConnectionDictionary.AddOrUpdate(playerName, Context.ConnectionId, (key, oldValue) => Context.ConnectionId);
-            ConnectionToPlayerDictionary.AddOrUpdate(Context.ConnectionId, playerName, (key, oldValue) => playerName);
-            return Task.CompletedTask;
-        }
-
-        public async Task GetAllGames ()
-        {
-            var games = Games.GetGames();
-            await Clients.Caller.AllGames(games);
-        }
-
-        public async Task GetAllMessage (GameInfo gameInfo)
-        {
-            if (gameInfo == null) return;
-            Game game = Games.GetGame(gameInfo.Id);
-            if (game != null)
-            {
-                List<CatanMessage> messages = new List<CatanMessage>();
-                messages.AddRange(game.GameLog.ToArray());
-                await Clients.Caller.AllMessages(messages);
-            }
-        }
-
-        public async Task GetPlayersInGame (Guid gameId)
-        {
-            Game game = Games.GetGame(gameId);
-            if (game != null)
-            {
-                await Clients.Caller.AllPlayers(game.NameToPlayerDictionary.Keys);
-            }
-            else
-            {
-                await Clients.Caller.AllPlayers(new List<string>());
-            }
-        }
-
-
-
-        public override async Task OnConnectedAsync ()
-        {
-            await Groups.AddToGroupAsync(Context.ConnectionId, AllUsers);
-            await base.OnConnectedAsync();
-        }
-
-        public override async Task OnDisconnectedAsync (Exception exception)
-        {
-            await Groups.RemoveFromGroupAsync(Context.ConnectionId, AllUsers);
-            if (ConnectionToPlayerDictionary.TryGetValue(Context.ConnectionId, out string playerName))
-            {
-                PlayerToConnectionDictionary.TryRemove(playerName, out string _);
-                ConnectionToPlayerDictionary.TryRemove(Context.ConnectionId, out string _);
-            }
-            await base.OnDisconnectedAsync(exception);
-        }
-        public Task SendPrivateMessage (string toName, CatanMessage message)
+        public Task SendPrivateMessage(string toName, CatanMessage message)
         {
             message.ActionType = ActionType.Redo;
             var toId = PlayerToConnectionDictionary[toName];
             // Console.WriteLine($"[ToId: {toId}] for [toName={toName}]");
             //return Clients.User(toId).ToOneClient(message);
             return Clients.All.ToOneClient(message);
-
         }
 
         #endregion Methods
@@ -249,9 +260,13 @@ namespace CatanHub
 
     public class NameUserIdProvider : IUserIdProvider
     {
+        #region Methods
+
         public string GetUserId(HubConnectionContext connection)
         {
             return connection.User?.Identity?.Name;
         }
+
+        #endregion Methods
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,46 +1,33 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Azure.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace CatanHub
 {
     public class Startup
     {
+        #region Properties
+
+        public IConfiguration Configuration { get; }
+
+        #endregion Properties
+
+        #region Constructors + Destructors
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        #endregion Constructors + Destructors
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddSignalR().AddAzureSignalR(options =>
-            {
-                options.ServerStickyMode = ServerStickyMode.Required;
-
-            }).AddHubOptions<CatanHub>(options =>
-            {
-                options.ClientTimeoutInterval = TimeSpan.FromMinutes(1);
-            });
-        
-        }
+        #region Methods
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -50,7 +37,6 @@ namespace CatanHub
                 app.UseDeveloperExceptionPage();
             }
 
-         
             app.UseRouting();
 
             app.UseAuthorization();
@@ -64,5 +50,19 @@ namespace CatanHub
                 endpoints.MapHub<CatanHub>("/CatanHub");
             });
         }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR().AddAzureSignalR(options =>
+            {
+                options.ServerStickyMode = ServerStickyMode.Required;
+            }).AddHubOptions<CatanHub>(options =>
+            {
+                options.ClientTimeoutInterval = TimeSpan.FromMinutes(1);
+            });
+        }
+
+        #endregion Methods
     }
 }
